cbdbd76ee3350b5b57c96c78e908383138f7ce04
mansi-wohlig <mansi.upadhyaya@wohlig.com>
2018-07-27 18:21:56 +0530

    chnages

---
+++ mongoConnection.js
@@ -0,0 +1,72 @@
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+var dbName = 'foodList';
+var URL = `mongodb://localhost:27017`
+
+function insertFoodItem(data, callback) {
+    //console.log(data);
+    MongoClient.connect(URL, {
+        useNewUrlParser: true
+    }, function (err, client) {
+        if (err) return console.log(err);
+        //  console.log(URL);
+        var db = client.db(dbName);
+        db.collection('food').insert(data, function (err, insertedData) {
+            callback({
+                err: err,
+                data: insertedData
+            });
+            client.close();
+        });
+    });
+}
+
+function getFoodList(callback) {
+    MongoClient.connect(URL, {
+        useNewUrlParser: true
+    }, function (err, client) {
+        if (err) return console.log(err);
+        //  console.log(URL);
+        var db = client.db(dbName);
+        db.collection("food").find({}).toArray(function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            callback({
+                err: err,
+                data: result
+            })
+            client.close();
+        });
+    });
+}
+
+function updateStatus(data, callback) {
+    MongoClient.connect(URL, {
+        useNewUrlParser: true
+    }, function (err, client) {
+        if (err) return console.log(err);
+        //  console.log(URL);
+        var db = client.db(dbName);
+        db.collection("food").findOneAndUpdate({
+            _id: ObjectID(data._id)
+        }, {
+            $inc: {
+                "created-till-now": 1
+            }
+        }, function (err, data) {
+            if (err) return console.log(err);
+            callback({
+                err: err,
+                data: data
+            });
+            client.close();
+
+        });
+    });
+}
+
+module.exports = {
+    insertFoodItem: insertFoodItem,
+    getFoodList: getFoodList,
+    updateStatus: updateStatus
+}
---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var mongoConnection = require('./mongoConnection');
+var bodyParser = require('body-parser')
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
+app.use(express.static(__dirname + '/node_modules'));
+//var io = require('socket.io').listen(3000);
+// io.on('connection', function (client) {});
+// io.listen(90);
+//configure socket
+// var WS = require("ws");
+// var WSS = new WS.Server({
+//     port: 90
+// });
+
+
+
+//creating apis
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/" + "index.html");
+});
+server.listen(80);
+
+
+
+app.post('/api/insertFoodItem', function (req, res) {
+    mongoConnection.insertFoodItem(req.body, function (data) {
+
+        res.json(data);
+    });
+});
+
+app.get('/api/getFoodList', function (req, res) {
+    mongoConnection.getFoodList(function (data) {
+        res.json(data);
+    });
+});
+app.post('/api/updateStatus', function (req, res) {
+    mongoConnection.updateStatus(req.body, function (data) {
+        res.json(data);
+        mongoConnection.getFoodList(function (data) {
+            if (!data.err) {
+                console.log(">>>>>>>>>", data.data);
+                io.emit('foodList', data.data);
+            }
+        });
+    });
+});
