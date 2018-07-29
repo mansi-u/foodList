fa59a20240fc54f4f3eed97d048c0da3dc50cb42
mansi-u <39210649+mansi-u@users.noreply.github.com>
2018-07-29 20:30:12 +0530

    changes

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.static(__dirname + '/node_modules'));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/" + "index.html");
 });
-server.listen(9000);
+server.listen(process.env.PORT || 5000);
 
 
 
